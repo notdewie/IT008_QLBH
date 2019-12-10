de651cbd2710dbb083b3b2f54a5aba86d05936c3
= <=>
2019-12-10 19:31:03 +0700

    Add login function

---
+++ SaleManages/SaleManages/SaleManages/DAO/AccountDAO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SaleManages.DAO
+{
+    public class AccountDAO
+    {
+        private static AccountDAO instance;
+
+        public static AccountDAO Instance
+        {
+            get { if (instance == null)
+                {
+                    instance = new AccountDAO();
+                }
+                return instance;
+            }
+            private set { instance = value; }
+        }
+        private AccountDAO(){}
+
+        public bool LoginCheck(string userName,string passWord)
+        {
+            string query = "SELECT * FROM TAIKHOAN WHERE TAIKHOAN = N'"+userName+"' AND MATKHAU = '"+passWord+"' ";
+            DataTable result = DataProvider.Instance.ExecuteQuery(query);
+            return result.Rows.Count>0;
+        }
+    }
+}
--- SaleManages/SaleManages/SaleManages/GUI/frmLogin.cs
+++ SaleManages/SaleManages/SaleManages/GUI/frmLogin.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SaleManages.DAO;
 
 namespace SaleManages
 {
@@ -25,12 +26,24 @@ namespace SaleManages
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            SalesManage f = new SalesManage();
-            this.Hide();
-            f.ShowDialog();
-            this.Show();
+            string userName = txbUserName.Text;
+            string passWord = txbPassWord.Text;
+            if (LoginCheck(userName,passWord))
+            {
+                SalesManage f = new SalesManage();
+                this.Hide();
+                f.ShowDialog();
+                this.Show();
+            }
+            else
+            {
+                MessageBox.Show("Sai tài khoản hoặc mật khẩu !", "Thông báo!", MessageBoxButtons.OK);
+            }
         }
-
+        bool LoginCheck(string userName,string passWord)
+        {
+            return AccountDAO.Instance.LoginCheck(userName,passWord);
+        }
         private void btnExit_Click(object sender, EventArgs e)
         {
             Application.Exit();
